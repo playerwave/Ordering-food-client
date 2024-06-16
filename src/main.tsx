1c4a6922c843e51bb5b4428fe1eba3a0c0e73d74
Thanaphat Ketsani <65160169@go.buu.ac.th>
2024-06-16 17:55:24 +0700

    create user request

--- src/main.tsx
+++ src/main.tsx
@@ -4,13 +4,24 @@ import ReactDOM from 'react-dom/client'
 import './global.css'
 import { BrowserRouter as Router } from 'react-router-dom'
 import Auth0ProvierWithNavigate from './auth/Auth0ProviderWithNavigate'
+import { QueryClient, QueryClientProvider } from 'react-query'
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  },
+}) 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Router>
-      <Auth0ProvierWithNavigate>
-        <AppRoutes/>
-      </Auth0ProvierWithNavigate>
+      <QueryClientProvider client={queryClient}>
+        <Auth0ProvierWithNavigate>
+          <AppRoutes/>
+        </Auth0ProvierWithNavigate>
+      </QueryClientProvider>
     </Router>
   </React.StrictMode>,
 )
