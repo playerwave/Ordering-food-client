a8766332a8531b35599ad15dc10076cbf4795c5d
Thanaphat Ketsani <65160169@go.buu.ac.th>
2024-06-21 05:55:20 +0700

    Pre-populate user profile form

--- src/pages/UserProfilePage.tsx
+++ src/pages/UserProfilePage.tsx
@@ -5,11 +5,21 @@ const UserProfilePage = () => {
   const { currentUser, isLoading: isGetLoading } = useGetMyUser();
   const { updateUser, isLoading: isUpdateLoading } = useUpdateMyUser();
 
-  if(isGetLoading) {
-    return <span>Loading...</span>
+  if (isGetLoading) {
+    return <span>Loading...</span>;
   }
 
-  return <UserProfileForm onSave={updateUser} isLoading={isUpdateLoading} />;
+  if (!currentUser) {
+    return <span>Unable to load user profile</span>;
+  }
+
+  return (
+    <UserProfileForm
+      currentUser={currentUser}
+      onSave={updateUser}
+      isLoading={isUpdateLoading}
+    />
+  );
 };
 
 export default UserProfilePage;
